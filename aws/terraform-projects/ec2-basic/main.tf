96e9d402105fbbf8967973d32bcd90cfdceb87c4
Chirag Shah <chiraginfosys@gmail.com>
2026-01-29 18:29:31 +1000

    Removed VM

--- aws/terraform-projects/ec2-basic/main.tf
+++ aws/terraform-projects/ec2-basic/main.tf
@@ -18,10 +18,6 @@ resource "aws_instance" "cms006" {
   instance_type = "t3.micro"
 }
 
-resource "aws_instance" "cms012" {
-  ami           = "ami-048ab8ac7e8c6533d"
-  instance_type = "t3.micro"
-}
 
 
 
