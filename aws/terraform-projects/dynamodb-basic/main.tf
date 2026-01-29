5faad337727184e7be13f912b3a3496ab44bbb82
Chirag Shah <chiraginfosys@gmail.com>
2026-01-29 17:39:49 +1000

    New item added

--- aws/terraform-projects/dynamodb-basic/main.tf
+++ aws/terraform-projects/dynamodb-basic/main.tf
@@ -1,18 +1,18 @@
 resource "aws_dynamodb_table" "cms001dynamodb" {
- name = "grocery_list"
- hash_key = "grocery_id"
- billing_mode = "PAY_PER_REQUEST"
- attribute {
-  name = "grocery_id"
-  type = "N"
-           }
- }
+  name         = "grocery_list"
+  hash_key     = "grocery_id"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "grocery_id"
+    type = "N"
+  }
+}
 
 resource "aws_dynamodb_table_item" "cms001dynamodb_item" {
- table_name = aws_dynamodb_table.cms001dynamodb.name
- hash_key = aws_dynamodb_table.cms001dynamodb.hash_key
+  table_name = aws_dynamodb_table.cms001dynamodb.name
+  hash_key   = aws_dynamodb_table.cms001dynamodb.hash_key
 
- item = <<ITEM
+  item = <<ITEM
  {
   "grocery_id": {"N": "1"},
   "grocery_item_name": {"S": "Rice"},
@@ -24,7 +24,7 @@ resource "aws_dynamodb_table_item" "cms001dynamodb_item" {
 
 resource "aws_dynamodb_table_item" "cms002dynamodb_item" {
   table_name = aws_dynamodb_table.cms001dynamodb.name
-  hash_key = aws_dynamodb_table.cms001dynamodb.hash_key
+  hash_key   = aws_dynamodb_table.cms001dynamodb.hash_key
 
   item = <<ITEM
   {
@@ -35,5 +35,20 @@ resource "aws_dynamodb_table_item" "cms002dynamodb_item" {
    "grocery_item_type": {"S": "Perishables"}
   }
  ITEM
- }
+}
+
+resource "aws_dynamodb_table_item" "cms003dynamodb_item" {
+  table_name = aws_dynamodb_table.cms001dynamodb.name
+  hash_key   = aws_dynamodb_table.cms001dynamodb.hash_key
+
+  item = <<ITEM
+  {
+   "grocery_id": {"N": "3"},
+   "grocery_item_name": {"S": "Eggs"},
+   "grocery_item_qty": {"N": "1"},
+   "grocery_item_price": {"N": "5.20"},
+   "grocery_item_type": {"S": "Perishables"}
+  }
+ ITEM
+}
 
