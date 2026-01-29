3f1949084a7e28b9562339276a6befdcd3a684b7
Chirag Shah <chiraginfosys@gmail.com>
2026-01-29 15:26:45 +1000

    Prevent duplicate creation

--- aws/terraform-projects/ec2-basic/main.tf
+++ aws/terraform-projects/ec2-basic/main.tf
@@ -17,3 +17,14 @@ resource "aws_instance" "cms006" {
   ami           = "ami-048ab8ac7e8c6533d"
   instance_type = "t3.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-tfstate-bucket"
+    key            = "envs/prod/ec2/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "my-tfstate-lock"
+    encrypt        = true
+  }
+}
+
