00f1caabacf7dc1550466eaef0e265bff88b8ebb
Chirag Shah <chiraginfosys@gmail.com>
2026-01-29 14:25:01 +1000

    Format main

--- aws/terraform-projects/ec2-basic/main.tf
+++ aws/terraform-projects/ec2-basic/main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "cms003"{
- ami = "ami-048ab8ac7e8c6533d"
- instance_type = "t3.micro"
+resource "aws_instance" "cms003" {
+  ami           = "ami-048ab8ac7e8c6533d"
+  instance_type = "t3.micro"
 }
 
-resource "aws_instance" "cms004"{
- ami = "ami-048ab8ac7e8c6533d"
- instance_type = "t3.micro"
+resource "aws_instance" "cms004" {
+  ami           = "ami-048ab8ac7e8c6533d"
+  instance_type = "t3.micro"
 }
 
