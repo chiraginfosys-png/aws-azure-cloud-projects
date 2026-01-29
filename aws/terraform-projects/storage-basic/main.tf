527c738f5fc8fd962371d030b4cb0d00f8db628b
Chirag Shah <chiraginfosys@gmail.com>
2026-01-29 11:50:30 +1000

    New Structure AWS

--- aws/terraform-projects/storage-basic/main.tf
+++ aws/terraform-projects/storage-basic/main.tf
@@ -1,3 +1,25 @@
 resource "aws_s3_bucket" "demo" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket" "cms001" {
+ bucket = "cshahbuck01"
+ object_lock_enabled = true
+ tags = {
+  Name = "marubucket"
+  Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "tstate_bucket001" {
+  bucket = "tstatebucket01"
+  tags = {
+   Description = "terraform state file backup"
+         }
+}
+
+resource "aws_s3_object" "cms001_object" {
+ bucket = aws_s3_bucket.cms001.id
+ content = "/Users/chiraginfossys/desktop/Terraform_Sample/invoice_7672023.pdf"
+ key = "invoice_7672023.pdf"
+}
